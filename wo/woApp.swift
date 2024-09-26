8e3708f3a40b7d516b03fc10fcc82f5a652acccb
Mkhaled00 <malduraywish24@twq.idserve.net>
2024-09-26 17:06:55 +0300

    Initial Commit

---
+++ wo/woApp.swift
@@ -0,0 +1,17 @@
+//
+//  woApp.swift
+//  wo
+//
+//  Created by muntaha khaled Alduraywish on 26/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct woApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ woTests/woTests.swift
@@ -0,0 +1,16 @@
+//
+//  woTests.swift
+//  woTests
+//
+//  Created by muntaha khaled Alduraywish on 26/09/2024.
+//
+
+import Testing
+
+struct woTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
